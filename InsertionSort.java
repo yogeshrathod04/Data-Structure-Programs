006effd882b48457526abccd81f492ba15b70312
Yogesh Rathod <117069440+yogeshrathod04@users.noreply.github.com>
2024-11-28 14:07:33 +0530

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -11,7 +11,7 @@ public class InsertionSort {
             arr[j+1] = current;
         }
         for(int i = 0; i<arr.length; i++){
-            System.out.print(arr[i]+ " ");
+            
         }
     }
 }
