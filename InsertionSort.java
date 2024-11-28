b9d94267782eb1748d424c8ff416b1e32a8428c5
Yogesh Rathod <117069440+yogeshrathod04@users.noreply.github.com>
2024-11-28 14:05:44 +0530

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,17 @@
+public class InsertionSort {
+    public static void main(String[] args) {
+        int arr[] = {29, 9, 23, 22, 7, 6, 78, 1};
+        for(int i=0; i<arr.length; i++){
+            int current = arr[i];
+            int j = i-1;
+            while (j >=0 && current < arr[j]) {
+                arr[j+1] = arr[j];    
+                j--;
+            } 
+            arr[j+1] = current;
+        }
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i]+ " ");
+        }
+    }
+}
