c13c70cb3df34c466aebcb02b2c9c097297f8069
Yogesh Rathod <117069440+yogeshrathod04@users.noreply.github.com>
2024-12-21 23:40:53 +0530

    Update ShellSort.java

--- ShellSort.java
+++ ShellSort.java
@@ -3,7 +3,7 @@ public class ShellSort {
         int i, j, n, gap, temp;
         int arr[] = { 23, 29, 15, 19, 31, 7, 9, 5, 2 };
         n = arr.length;
-        System.out.println("Name: Yogesh Rathod \nRoll No: C24092\n");
+        System.out.println("Name: Yogesh Rathod \n");
         System.out.println("Array Elements are:");
         for (i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
