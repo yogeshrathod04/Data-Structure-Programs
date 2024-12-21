f73be64432d7c9e9627a67f073814136b1c80bf7
Yogesh Rathod <117069440+yogeshrathod04@users.noreply.github.com>
2024-12-21 23:40:09 +0530

    Add files via upload

---
+++ ShellSort.java
@@ -0,0 +1,30 @@
+public class ShellSort {
+    public static void main(String args[]) {
+        int i, j, n, gap, temp;
+        int arr[] = { 23, 29, 15, 19, 31, 7, 9, 5, 2 };
+        n = arr.length;
+        System.out.println("Name: Yogesh Rathod \nRoll No: C24092\n");
+        System.out.println("Array Elements are:");
+        for (i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        for (gap = n / 2; gap >= 1; gap /= 2) {
+            for (j = gap; j < n; j++) {
+                for (i = j - gap; i >= 0; i -= gap) {
+                    if (arr[i + gap] > arr[i]) {
+                        break;
+                    } else {
+                        temp = arr[i + gap];
+                        arr[i + gap] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+        }
+        System.out.println("\nArray Elements after Shell Sort are:");
+        for (i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
